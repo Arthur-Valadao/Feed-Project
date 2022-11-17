7a9e4e8620de40ab24cdeeb6d0e2233fb0ca05ad
Arthur <Arth.valadao@gmail.com>
2022-11-16 23:18:33 -0400

    insert Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-
-import { Header } from './components/Header';
-import './global.css';
-import styles from './App.module.css';
-import { Sidebar } from './components/Sidebar';
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Posts } from "./components/Posts/Posts";
+import "./global.css";
+import styles from "./App.module.css";
 
 function App() {
-
   return (
     <div>
       <Header />
-      
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-
+          <Posts />
+          <Posts />
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,35 @@
+import styles from "./Posts.module.css";
+
+export function Posts() {
+  return (
+    <article className={styles.posts}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/Arthur-Valadao.png" />
+          <div className={styles.authorInfo}>
+            <strong>Arthur Valadão</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="16 de Novembro às 23:07" dateTime="2022-11-16 23:07:15">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat </a>
+        </p>
+      </div>
+    </article>
+  );
+}
