45fc064e3a4e0e79fcc1b0be2ce503017c460692
Arthur <Arth.valadao@gmail.com>
2022-11-16 22:06:44 -0400

    insert Phosphor icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,12 +10,20 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/Arthur-Valadao.png"
+        />
+
         <strong>Arthur Valadão</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
